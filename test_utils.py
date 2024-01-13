4869152544c0155a84b9d1cdbe3a92940698bbf5
Justin Gu <77459359+JusGu@users.noreply.github.com>
2024-01-13 15:13:13 -0500

    increased test cases

--- test_utils.py
+++ test_utils.py
@@ -2,7 +2,7 @@ from utils import create_shortname
 
 def test_create_shortname():
     assert create_shortname("arts") == "arts"
-    assert create_shortname("uw arts") == "arts"
+    assert create_shortname("uw   arts") == "arts"
     assert create_shortname("uw arts club") == "arts-club"
     assert create_shortname("uw arts crafts club") == "arts-crafts-club"
     assert create_shortname("Waterloo Class of '25") == "class-of-25"
@@ -10,3 +10,5 @@ def test_create_shortname():
     assert create_shortname("Physclub") == "physclub"
     assert create_shortname("OSU!Uwaterloo") == "osu"
     assert create_shortname("UW/WLU DD '25") == "dd-25"
+    assert create_shortname("uwclubs") == "uwclubs"
+    assert create_shortname("uw clubs") == "clubs"
